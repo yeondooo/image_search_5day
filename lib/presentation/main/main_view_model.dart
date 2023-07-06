87040645e1477d77e6b39575f85673cc0d20852f
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-07 01:07:25 +0900

    feat: copyWith 메서드 활용하여 main view model 구현

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:image_search_5day/domain/use_case/get_top_five_most_viewd_images_use_case.dart';
+import 'package:image_search_5day/presentation/main/main_state.dart';
 
-class MainViewModel with ChangeNotifier {}
+class MainViewModel with ChangeNotifier {
+  final GetTopFiveMoseViewdImagesUseCase _getTopFiveMoseViewdImagesUseCase;
+
+  MainViewModel(this._getTopFiveMoseViewdImagesUseCase);
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  void fetchImages(String query) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    _state = state.copyWith(
+        photos: await _getTopFiveMoseViewdImagesUseCase.execute(query),
+        isLoading: false);
+    notifyListeners();
+  }
+}

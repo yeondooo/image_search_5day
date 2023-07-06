cd3f59ec19d7ae42917114306b2c9af460625db8
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-07 01:38:22 +0900

    style: safeArea, 안내 문구 추가

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -28,52 +28,63 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: const Text('이미지 검색 앱'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: '검색어를 입력하세요',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      viewModel.fetchImages(controller.text);
+                    },
+                  ),
                 ),
-                hintText: '검색어를 입력하세요',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    viewModel.fetchImages(controller.text);
+              ),
+              if (state.photos.isEmpty)
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text('검색 결과는 조회수 순으로 10개가 정렬됩니다.'),
+                ),
+              const SizedBox(height: 8),
+              Expanded(
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: state.photos.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                  ),
+                  itemBuilder: (context, index) {
+                    final photo = state.photos[index];
+                    return Hero(
+                      tag: photo.id,
+                      child: GestureDetector(
+                        onTap: () {
+                          context.push('/detail', extra: photo);
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            photo.webformatURL,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
                   },
                 ),
-              ),
-            ),
-            Expanded(
-              child: GridView.builder(
-                shrinkWrap: true,
-                itemCount: state.photos.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 8,
-                  mainAxisSpacing: 8,
-                ),
-                itemBuilder: (context, index) {
-                  final photo = state.photos[index];
-                  return Hero(
-                    tag: photo.id,
-                    child: GestureDetector(
-                      onTap: () {
-                        context.push('/detail', extra: photo);
-                      },
-                      child: Image.network(
-                        photo.webformatURL,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );

08b58fef83973e39b76726060133745ecac48e58
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-07 00:23:25 +0900

    feat: 클린 아키텍처 기반으로 폴더 뼈대 작업

---
+++ lib/presentation/detail/detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('디테일 페이지'),
+      ),
+      body: Container(),
+    );
+  }
+}
